c62e8b54475c2e53889eb18300c28452e511be15
Marco Roman <marcoroman77@yahoo.com>
2017-11-27 18:45:47 -0800

    Reorganized code for clarity.

--- CS301Project3/src/Interpolator.java
+++ CS301Project3/src/Interpolator.java
@@ -7,10 +7,20 @@ import java.util.Scanner;
 
 public class Interpolator {
 	public static void main(String[] args) throws FileNotFoundException{
-		File inputFile = new File("input.txt");
-		File outputFile = new File("output.txt");
 		
-		PrintWriter writer = new PrintWriter(outputFile);
+		ArrayList<ArrayList<Float>> dividedDifferenceTable = new ArrayList<>();
+		
+		//Generating the divided differences based on input data
+		createDividedDifferenceTable(dividedDifferenceTable);
+
+		display(dividedDifferenceTable);
+	}
+	
+	//Divided difference table stored as an ArrayList of ArrayLists
+	//Generated based on provided input file
+	public static void createDividedDifferenceTable(ArrayList<ArrayList<Float>> table) throws FileNotFoundException{
+		
+		File inputFile = new File("input.txt");
 		Scanner reader = new Scanner(inputFile);
 		
 		ArrayList<Float> data = new ArrayList<>();
@@ -21,49 +31,27 @@ public class Interpolator {
 		
 		reader.close();
 		
-		ArrayList<ArrayList<Float>> dividedDifferenceTable = new ArrayList<>();
-		
 		//First ArrayList in the table stores x values
-		dividedDifferenceTable.add(new ArrayList<Float>());
-		dividedDifferenceTable.get(0).addAll(0, data.subList(0, data.size() / 2));
-		
+		table.add(new ArrayList<Float>());
+		table.get(0).addAll(0, data.subList(0, data.size() / 2));
+				
 		//Second ArrayList in the table stores functional values
-		dividedDifferenceTable.add(new ArrayList<Float>());
-		dividedDifferenceTable.get(1).addAll(0, data.subList(data.size() / 2, data.size()));
+		table.add(new ArrayList<Float>());
+		table.get(1).addAll(0, data.subList(data.size() / 2, data.size()));
 		
-		//Generating the divided differences based on input data
-		createDividedDifferenceTable(dividedDifferenceTable);
-		
-		//Test display of values to output file
-		for(int i = 0; i < dividedDifferenceTable.size(); ++i)
-			writer.println(Arrays.toString(dividedDifferenceTable.get(i).toArray()));
-		
-		//Display of interpolating polynomial to output file
-		writer.println("\nInterpolating polynomial is:");
-		writer.println(display(dividedDifferenceTable));
-		
-		//Display of simplified interpolating polynomial to output file
-		writer.println("\nSimplified polynomial is:");
-		
-		writer.close();
-	}
-	
-	//Divided difference table stored as an ArrayList of ArrayLists
-	public static void createDividedDifferenceTable(ArrayList<ArrayList<Float>> table){
-		
+		//Iteratively filling in the table
 		for(int i = 0; i < table.get(0).size() - 1; ++i){
 			table.add(new ArrayList<Float>());
 			
 			//Divided difference calculation
-			for(int j = 0; j < table.get(0).size() - (i + 1); ++j){
+			for(int j = 0; j < table.get(0).size() - (i + 1); ++j)
 				table.get(table.size() - 1).add((table.get(i + 1).get(j + 1) - table.get(i + 1).get(j)) / 
 						(table.get(0).get(j + (i + 1)) - table.get(0).get(j)));
-			}
 		}
 	}
 	
 	//Storing the interpolating polynomial before simplification as a string
-	public static String display(ArrayList<ArrayList<Float>> table){
+	public static String generatePolynomial(ArrayList<ArrayList<Float>> table){
 		String polynomial = "";
 		
 		for(int i = 1; i < table.size(); ++i){
@@ -92,5 +80,23 @@ public class Interpolator {
 		}
 		
 		return polynomial;
+	}
+	
+	public static void display(ArrayList<ArrayList<Float>> table) throws FileNotFoundException{
+		File outputFile = new File("output.txt");
+		PrintWriter writer = new PrintWriter(outputFile);
+		
+		//Test display of values to output file
+		for(int i = 0; i < table.size(); ++i)
+			writer.println(Arrays.toString(table.get(i).toArray()));
+				
+		//Display of interpolating polynomial to output file
+		writer.println("\nInterpolating polynomial is:");
+		writer.println(generatePolynomial(table));
+				
+		//Display of simplified interpolating polynomial to output file
+		writer.println("\nSimplified polynomial is:");
+		
+		writer.close();
 	}
 }
